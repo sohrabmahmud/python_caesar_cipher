7d96acd6ca8dfd8a1fc17e682264f3a3821bd8a9
sohrabmahmud <sohrabmahmud@gmail.com>
2024-11-24 12:53:10 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-# TODO-1: Import and print the logo from art.py when the program starts.
 import art
 print(art.logo)
 
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-# TODO-2: What happens if the user enters a number/symbol/space?
 
 
 def caesar(original_text, shift_amount, encode_or_decode):
@@ -23,7 +20,6 @@ def caesar(original_text, shift_amount, encode_or_decode):
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
 
-# TODO-3: Can you figure out a way to restart the cipher program?
 should_continue = True
 
 while should_continue:
